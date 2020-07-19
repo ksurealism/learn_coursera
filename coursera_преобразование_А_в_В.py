f148d38c1bb627d37801dc6f2a611e0a95e06014
ksu <ksurealism@gmail.com>
2020-07-19 22:56:48 +0800

    написала функцию преобразования a в b

--- coursera_преобразование_А_в_В.py
+++ coursera_преобразование_А_в_В.py
@@ -1,11 +1,13 @@
-a = int(input())
-b = int(input())
-c = a
-i = 0
-while i != b:
-    if c % 2 == 0:
-        c = a / 2
-    else:
-        c = a - 1
-    i += 1
-print(c)
+def conversion_a_to_b():
+    a = int(input())
+    b = int(input())
+    while a > b:
+        if (a % 2 == 0) and (a >= (b * 2)):
+            a = a / 2
+            print(":2")
+        else:
+            a = a - 1
+            print("-1")
+
+
+conversion_a_to_b()
