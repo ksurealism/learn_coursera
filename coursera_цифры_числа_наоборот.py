cfcf9e3a44abb831746e59d5bc70f647aac747ae
ksu <ksurealism@gmail.com>
2020-07-20 03:16:31 +0800

    написала функцию преобразования a в b

---
+++ coursera_цифры_числа_наоборот.py
@@ -0,0 +1,10 @@
+def conversion_a_to_b():
+    n = int(input())
+    new_num = 0
+    while n > 0:
+        new_num = (new_num * 10) + (n % 10)
+        n = n // 10
+    print(new_num)
+
+
+conversion_a_to_b()
